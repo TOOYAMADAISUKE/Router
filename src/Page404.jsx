c5b2dfd487bc210001d94b1a0877c673501b5138
TOOYAMADAISUKE <57941111+TOOYAMADAISUKE@users.noreply.github.com>
2021-03-28 22:34:17 +0900

    追加したよ

--- src/Page404.jsx
+++ src/Page404.jsx
@@ -4,9 +4,7 @@ export const Page404 = () => {
   return (
     <div>
       <h1>ページが見つかりません</h1>
-      <Link to="/" />
-      Topに戻る
-      <Link />
+      <Link to="/">Topに戻る</Link>
     </div>
   );
 };
